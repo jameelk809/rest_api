392c968f5ba84cf42dd2f6bcf860076cded4c1f1
Kamran <jameelk809@gmail.com>
2022-05-16 11:25:03 +0530

    model created and registered

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . models import employees
 
 # Register your models here.
+admin.site.register(employees)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+class employees(models.Model):
+    firstname = models.CharField(max_length=10)
+    lastname = models.CharField(max_length=10)
+    emp_id = models.IntegerField()
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
+        
